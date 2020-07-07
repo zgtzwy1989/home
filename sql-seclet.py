90a1be66a9e6c8b3cba3b54eff043164a91179eb
zgtzwy1989 <89235983@qq.com>
2020-07-08 01:01:17 +0800

    "完成更具模糊查询结果"

--- sql-seclet.py
+++ sql-seclet.py
@@ -3,21 +3,32 @@ import tkinter.messagebox
 from PIL import Image
 from PIL import ImageTk
 from tkinter import StringVar
-
-
+import pymysql
 def reply():##弹窗窗口
-	p=StringVar()
+	
+	s=e.get()
+	print(s)
+	conn = pymysql.connect(host='localhost', port=3306, user='root', 
+            passwd='cq123456', db='农行业务', charset='utf8')
+	cour=conn.cursor()
+	effect_cour=cour.execute(f"SELECT * FROM 工作表 WHERE id LIKE '%{s}%'")
+	p=cour.fetchone()
+	p0=p[0]
+	p1=p[1]
+	p2=p[2]
+	cour.close()
 	sql1=tk.Toplevel()
 	sql1.title("查询软件")
 	sql1.geometry("400x300")
-	id_label_min=tk.Label(sql1,text=f"{e.get()}").place(x=0,y=10)
-	p=StringVar()
-	p.set(e.get())
-	p=StringVar()
-	id_input=tk.Entry(sql1,text=f"{e.get()}",textvariable=p).place(x=20,y=10)
+	id_label_min=tk.Label(sql1,text=f"id:").place(x=0,y=10)
+	id_man=StringVar()
+	id_man.set(p0)
+	id_input_min=tk.Entry(sql1,textvariable=id_man).place(x=20,y=10)
 	name_label_min=tk.Label(sql1,text="商户名称").place(x=120,y=10)
-	#name_input_min=tk.Entry(sql1).place(x=180,y=10)
-	img=Image.open(r"C:\\Users\\Administrator\\Desktop\\timg.jpg")#图片地址
+	name_man=StringVar()
+	name_man.set(p1)
+	name_input_min=tk.Entry(sql1,textvariable=name_man).place(x=180,y=10)
+	img=Image.open(r"C:\\Users\\Administrator\\Desktop\\{}".format(p2))#图片地址
 	img=img.resize((300,200),Image.ANTIALIAS)
 	img = ImageTk.PhotoImage(img)
 	sss=tk.Label(sql1,image=img).place(x=0,y=60)
