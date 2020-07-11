6b0db84e579ca264ba97a16354a2d6139540f9ff
zgtzwy1989 <89235983@qq.com>
2020-07-11 23:21:27 +0800

    完成根据ID查询操作

--- sql-seclet.py
+++ sql-seclet.py
@@ -7,19 +7,20 @@ import pymysql
 def reply():##弹窗窗口
 	
 	s=e.get()
-	print(s)
+	
 	conn = pymysql.connect(host='localhost', port=3306, user='root', 
-            passwd='cq123456', db='农行业务', charset='utf8')
+            passwd='cq123456', db='业务', charset='utf8')
 	cour=conn.cursor()
-	effect_cour=cour.execute(f"SELECT * FROM 工作表 WHERE id LIKE '%{s}%'")
+	effect_cour=cour.execute(f"SELECT * FROM world WHERE Merchant_id LIKE '%{s}%'")
 	p=cour.fetchone()
+	print(p)
 	p0=p[0]
 	p1=p[1]
 	p2=p[2]
 	cour.close()
 	sql1=tk.Toplevel()
 	sql1.title("查询软件")
-	sql1.geometry("400x300")
+	sql1.geometry("600x500")
 	id_label_min=tk.Label(sql1,text=f"id:").place(x=0,y=10)
 	id_man=StringVar()
 	id_man.set(p0)
@@ -28,8 +29,8 @@ def reply():##弹窗窗口
 	name_man=StringVar()
 	name_man.set(p1)
 	name_input_min=tk.Entry(sql1,textvariable=name_man).place(x=180,y=10)
-	img=Image.open(r"C:\\Users\\Administrator\\Desktop\\{}".format(p2))#图片地址
-	img=img.resize((300,200),Image.ANTIALIAS)
+	img=Image.open(r"E:\\img\\{}.jpg".format(p2))
+	img=img.resize((500,400),Image.ANTIALIAS)
 	img = ImageTk.PhotoImage(img)
 	sss=tk.Label(sql1,image=img).place(x=0,y=60)
 	sql1.mainloop()
@@ -43,6 +44,5 @@ id_input=tk.Entry(sql,textvariable=e).place(x=60,y=0)
 name_label=tk.Label(text="商户名称:").place(x=0,y=30)
 name_input=tk.Entry().place(x=60,y=30)
 seclet_button=tk.Button(sql,text="查询",command=reply).place(x=300,y=30)
+
 sql.mainloop()
-
-
