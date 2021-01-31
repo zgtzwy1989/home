b59411da5b4aaf40a1de2c89c7fbb975b1485261
zgtzwy1989 <89235983@qq.com>
2021-02-01 06:23:25 +0800

    减少输入量

--- 公司软件/录入界面.py
+++ 公司软件/录入界面.py
@@ -1,5 +1,5 @@
 import sys,pymysql
-from PyQt5.QtWidgets import QApplication,QWidget,QHBoxLayout,QVBoxLayout,QLabel,QLineEdit,QPushButton,QMessageBox
+from PyQt5.QtWidgets import QApplication,QWidget,QHBoxLayout,QVBoxLayout,QLabel,QLineEdit,QPushButton,QMessageBox,QComboBox,QRadioButton
 class Demo (QWidget):
     def __init__(self):
         super(Demo, self).__init__()
@@ -7,13 +7,18 @@ class Demo (QWidget):
         self.setWindowTitle("录入系统")
         self.ql_tsbq=QLabel("今天电话数",self)#标签数
         self.ql_sh=QLabel("企业税号",self)#税号
-        self.ql_zw=QLabel("职位",self)#职位
+        self.ql_zw=QLabel("职位：",self)#职位
+        self.ql_zw_edit = QComboBox(self)
+        self.ql_zw_edit.addItems(["法人", "财务", "办税人", "办盘人", "发票联系人"])
         self.ql_phone=QLabel("电话",self)#电话
         self.ql_content=QLabel("内容",self)#内容
         self.ql_sh_edit=QLineEdit(self)
-        self.ql_zw_edit = QLineEdit(self)
         self.ql_phone_edit = QLineEdit(self)
         self.ql_content_edit = QLineEdit(self)
+        self.ql_yxx = QLabel("有效性",self)
+        self.ql_yxx_yes_edit=QRadioButton("有效")#有效性
+        self.ql_yxx_no_edit = QRadioButton("无效")#有效性
+        self.ql_yxx_yes_edit.setChecked(True)
         self.qr_button=QPushButton("确认",self)
         self.qc_button = QPushButton("清除",self)
 
@@ -30,6 +35,11 @@ class Demo (QWidget):
         self.v4 = QHBoxLayout()
         self.v4.addWidget(self.ql_content)
         self.v4.addWidget(self.ql_content_edit)
+        self.v5 = QHBoxLayout()
+        self.v5.addWidget(self.ql_yxx)
+        self.v5.addWidget(self.ql_yxx_yes_edit)
+        self.v5.addWidget( self.ql_yxx_no_edit)
+
 # 按钮横向排序h2
         self.h2 = QHBoxLayout()
         self.h2.addWidget(self.qr_button)
@@ -40,6 +50,7 @@ class Demo (QWidget):
         self.zv1.addLayout(self.v2)
         self.zv1.addLayout(self.v3)
         self.zv1.addLayout(self.v4)
+        self.zv1.addLayout(self.v5)
         self.zv1.addLayout(self.h2)
         self.setLayout(self.zv1)
         self.qr_button.clicked.connect(self.inster)
@@ -53,20 +64,32 @@ class Demo (QWidget):
 
     def inster(self):
         sh=self.ql_sh_edit.text()
-        zw=self.ql_zw_edit.text()
-        phone=self.ql_phone_edit.text()
-        nr=self.ql_content_edit.text()
+        zw=self.ql_zw_edit.currentText()
         
-        sql=f"insert into 电话联系表(`税号`,联系人,`电话`,content)value('{sh}','{zw}','{phone}','{nr}')"
-        print(sql)
+        phone=self.ql_phone_edit.text()
+
+        nr=self.ql_content_edit.text()
+        if self.ql_yxx_yes_edit.isChecked() == True:
+            yxx = "有效"
+            sql = f"insert into 电话联系表(`税号`,联系人,`电话`,content,有效性)value('{sh}','{zw}','{phone}','{nr}','{yxx}')"
+        if self.ql_yxx_no_edit.isChecked() == True:
+            yxx = "无效"
+            sql = f"insert into 电话联系表(`税号`,联系人,`电话`,content,有效性)value('{sh}','{zw}','{phone}','{nr}','{yxx}')"
+        
+
+        
+            
+           
         self.i(sql)
+
         QMessageBox.information(self, "消息框标题", "保存成功。", QMessageBox.Yes)
-        self.ql_zw_edit.clear()
+        self.ql_yxx_yes_edit.setChecked(True)
         self.ql_phone_edit.clear()
         self.ql_content_edit.clear()
+       
     def clear(self):
         self.ql_sh_edit.clear()
-        self.ql_zw_edit.clear()
+       
         self.ql_phone_edit.clear()
         self.ql_content_edit.clear()
 
