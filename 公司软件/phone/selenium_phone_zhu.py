a839e576949a4d06d6e21237ffaba3a031faab51
zgtzwy1989 <89235983@qq.com>
2021-03-30 15:52:58 +0800

    清库

--- 公司软件/phone/selenium_phone_zhu.py
+++
@@ -1,4 +0,0 @@
-import selenium.webdriver
-dirves=selenium.webdriver.Chrome(r"D:\chromedriver.exe")
-dirves.get(r"http://192.168.2.16:8089/ZHYW//res/mainui/page/index.html?fromForm=a8login")
-user_name=dirves.find_element_by_id("user")
--- 公司软件/电话催费软件/sqlfile.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-class htxx_sql():
-    def __init__(self):
-        self.new_sql=pymysql.connect(host="localhost",passwd="cq123456",user="root",db="业务",port=3306)
-        self.bd=self.new_sql.cursor()
-    def sql_insert(self,sql):
-        self.bd.execute(sql)
-        self.new_sql.commit()
-        self.new_sql.close()
